58389629a29da8c1b3c21779bda56e3a9de1b633
dayquark <alexmepejd@hotmail.com>
2024-09-12 13:27:13 -0400

    updates
    updates

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,5 +1,14 @@
 var _ = require('lodash')
+var fs = require('fs')
+var data = require('./data.json')
+
+console.log(data.name)
 
 var msg = "Hello Node JS";
 console.log(msg);
-console.log(_.random(10, 200))
+console.log(_.random(10, 20))
+
+fs.readFile('./data.json', 'utf-8', function(err, data){
+    var data = JSON.parse(name)
+    console.log(data)
+})
